8748310438770f675d8f6f8978a8df4bc4f228a7
archlinux <hanffeix@outlook.com>
2022-12-21 23:57:06 +0800

    2022/12/21

---
+++ cpp/438.找到字符串中所有字母异位词.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=438 lang=cpp
+ * @lcpr version=21104
+ *
+ * [438] 找到字符串中所有字母异位词
+ */
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+using namespace std;
+// @lc code=start
+class Solution {
+  public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> res;
+        vector<int> arr(26);
+        for (int i = 0; i < p.size(); ++i) {
+            ++arr[p[i] - 'a'];
+        }
+
+        for (int l = 0, r = 0; r < s.size(); ++r) {
+            --arr[s[r] - 'a'];
+            while (arr[s[r] - 'a'] < 0) {
+                ++arr[s[l] - 'a'];
+                ++l;
+            }
+            if (r - l + 1 == p.size())
+                res.push_back(l);
+        }
+        return res;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// "cbaebabacd"\n"abc"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "abab"\n"ab"\n
+// @lcpr case=end
+
+ */
