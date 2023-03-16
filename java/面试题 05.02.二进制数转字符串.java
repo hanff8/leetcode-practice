dbee4ea83c661b7f2164fffd6f7ef02f25b143cd
PC_MSI <hanffeix@outlook.com>
2023-03-16 22:48:15 +0800

    update

--- java/面试题 05.02.二进制数转字符串.java
+++ java/面试题 05.02.二进制数转字符串.java
@@ -1,5 +1,6 @@
 /*
  * @lc app=leetcode.cn id=面试题 05.02 lang=java
+ * 
  * @lcpr version=21708
  *
  * [面试题 05.02] 二进制数转字符串
@@ -8,7 +9,19 @@
 // @lc code=start
 class Solution {
     public String printBin(double num) {
-
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        sb.append("0.");
+        while (num != 0) {
+            num *= 2;
+            int digital = (int) num;
+            sb.append(digital);
+            num -= digital;
+            count++;
+            if (count > 32)
+                return "ERROR";
+        }
+        return sb.toString();
     }
 }
 // @lc code=end
@@ -21,14 +34,10 @@ class Solution {
 
 
 /*
-// @lcpr case=start
-// 
-// @lcpr case=end
-
-// @lcpr case=start
-// 
-// @lcpr case=end
-
+ * // @lcpr case=start // // @lcpr case=end
+ * 
+ * // @lcpr case=start // // @lcpr case=end
+ * 
  */
 
 
